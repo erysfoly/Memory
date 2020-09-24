9cb49980e81cf738acb1a63f198832f3aa99dceb
Chris <62357935+YouknowNothingJeanNeige@users.noreply.github.com>
2020-09-24 16:59:27 +0200

    generate cards and shuffle them

--- main.js
+++ main.js
@@ -1,58 +1,49 @@
 (function () {
+    var main = document.getElementById('main');
+    window.customElements.define('memory-card', Card)
+
     let tabColors = ['red', 'darkred', 'yellow', 'green', 'grey', 'pink', 'chartreuse',
         'blueviolet', 'blue', 'darkorange', 'fuchsia', 'lightgray'];
 
     let memoryCLicked = null;
+
     function getColor() {
-           return tabColors[randomize(tabColors.length, 0)];
+        return tabColors[randomize(tabColors.length, 0)];
     }
 
     function randomize(base, minimum) {
         return Math.floor(Math.random() * base) + minimum;
     }
 
-
-
-    var main = document.getElementById('main');
-    window.customElements.define('memory-card', Card)
-
-
-    var color = getColor();
-    var card = new Card(color);
-
-    var color2 = getColor();
-    var card2 = new Card(color2);
-
-    card.addEventListener('cardClicked', function (ev) {
-        console.log(ev);
-        checkMemory(ev.detail);
-    });
-    card2.addEventListener('cardClicked', function (ev) {
-        console.log(ev);
-        checkMemory(ev.detail);
-    });
-
-    function checkMemory(color){
-        if ( !memoryCLicked ){
-            // 1er click
-            memoryCLicked = color;
-        }else{
-            // 2iem click
-            // ??????? PAIR ???????
-            if ( memoryCLicked === color) {
-
-                // PAIRE
-                // ON BOUGE PAS LES CARD
-            }else {
-                // PAS UNE PAIRE<<<<<
-                //ON CACHE LES 2
-            }
-            memoryCLicked = null;
-        }
+    var niv1 = 6;
+    var niv2 = 12;
+    var niv3 = 24;
+    var cards = [];
+    
+    for (var i = 0; i < niv2 / 2; i++) {
+        var color = getColor();
+        cards.push(new Card(color), new Card(color));
     }
 
-    main.appendChild(card);
-    main.appendChild(card2);
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    cards = shuffle(cards);
+    for (var i = 0; i < cards.length; i++) {
+        var card = cards[i];
+        main.appendChild(card);
+        card.addEventListener('cardClicked', function (ev) {
+            console.log(ev);
+        });
+    }
 
 
 })();
