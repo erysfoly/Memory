26e195aa92f542409c947486f646c2f5c715427c
Chris <62357935+YouknowNothingJeanNeige@users.noreply.github.com>
2020-09-24 11:31:28 +0200

    event listener

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
     var main = document.getElementById('main');
     window.customElements.define('memory-card', Card)
     var card = new Card();
+    card.addEventListener('cardClicked', function(ev){
+        console.log(ev);
+    });
+
     main.appendChild(card);
 
 })();
