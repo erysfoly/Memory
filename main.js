6b495c7f65554c66f59d69bf89c0869a2031399a
Cindy Chiffoleau <64546974+erysfoly@users.noreply.github.com>
2020-09-24 22:07:49 +0200

    front bien avancé

--- main.js
+++ main.js
@@ -1,40 +1,68 @@
 (function () {
 	
 	var main = document.getElementById('main');
+
+	var screenMenu = document.getElementById('screen-menu');
+	var screenGame = document.getElementById('screen-game');
+	var screenEnd = document.getElementById('screen-end');
+
+	var btnStartGame = document.getElementById("start");
+
+	var btnEndGame = document.getElementById("end");
+
+	var msgResult = document.getElementById('result');
+	var resultTitle = document.getElementById('result-title')
+	var btnRetry = document.getElementById('retry');
+
 	window.customElements.define('memory-card', Card);
+	var card = new Card();
+
+	var victory; //bool
 	
 	initGame();
 	
 	function initGame() {
-		var btnStartGame = document.getElementById("start");
+		screenMenu.setAttribute("style","display:block");
+
 		btnStartGame.onclick = function(){
-			var screenMenu = document.getElementById('screen-menu');
 			screenMenu.setAttribute("style","display:none");
 			inGame();
 		};
 	}
 	
 	function inGame() {
-		var screenGame = document.getElementById('screen-game');
 		screenGame.setAttribute("style","display:block");
-		var card = new Card();
+
 		card.setAttribute("class","rounded-lg");
 		card.addEventListener('cardClicked', function(ev){
 			console.log(ev);
 		});
-		main.appendChild(card);
+		screenGame.appendChild(card);
 		
-		/*var btnEndGame = document.getElementById("end");
 		btnEndGame.onclick = function(){
-			var screenGame = document.getElementById('screen-game');
 			screenGame.setAttribute("style","display:none");
+			victory = true;
 			endGame();
-		};*/
+		};
 	}
 	
 	function endGame() {
-		var screenEnd = document.getElementById('screen-end');
+		screenGame.removeChild(card);
 		screenEnd.setAttribute("style","display:block");
+
+		if (victory == true) {
+			resultTitle.innerText = "Congrats!";
+			btnRetry.innerText = "Play again";
+
+		} else {
+			resultTitle.innerText = "Game Over!";
+			btnRetry.innerText = "Retry";
+		}
+
+		btnRetry.onclick = function(){
+			screenEnd.setAttribute("style","display:none");
+			initGame();
+		};
 	}
 
 })();
