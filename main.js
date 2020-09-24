829b5e6b44a3fa8457d5f81099588bb29a04ee6e
Cindy Chiffoleau <64546974+erysfoly@users.noreply.github.com>
2020-09-24 11:32:19 +0200

    working progress

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 (function () {
-    var main = document.getElementById('main');
-    window.customElements.define('memory-card', Card)
-    var card = new Card();
-    main.appendChild(card);
+	
+	var main = document.getElementById('main');
+	window.customElements.define('memory-card', Card);
+	init();
+	
+	function init () {
+		var btnStartGame = document.getElementById("start");
+		btnStartGame.onclick = function(){
+			var screenMenu = document.getElementById('screen-menu');
+			screenMenu.setAttribute("style","display:none");
+			ingame();
+		};
+	}
+	
+	function ingame() {
+		var screenGame = document.getElementById('screen-game');
+		screenGame.setAttribute("style","display:block");
+		var card = new Card();
+		main.appendChild(card);
+	}
+	
+	function end() {
+		var screenEnd = document.getElementById('screen-end');
+		screenEnd.setAttribute("style","display:block");
+	}
+	
 
 })();
 
