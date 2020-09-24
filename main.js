16b2ed5e62c505ed0f073c5c46bcbbfe9d81b83f
Cindy Chiffoleau <64546974+erysfoly@users.noreply.github.com>
2020-09-24 09:55:52 +0200

    test

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+	
+	/*function showDifficultyButton () {
+		
+	}
+
+	function chooseDifficulty () {
+		
+	}*/
+	
+	class Card extends HTMLElement {
+		constructor() {
+			// Always call super first in constructor
+			super();
+
+			// Element functionality written in here
+
+		}
+}
+	
+window.customElements.define("memory-card", Card);
+	
+document.body.appendChild();
+	
+
