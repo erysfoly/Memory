4e60a9ff1536085b3a60208abbc950aeec3eff6a
Chris <62357935+YouknowNothingJeanNeige@users.noreply.github.com>
2020-09-24 19:58:20 +0200

    little correction

--- Controller/BoardController.js
+++ Controller/BoardController.js
@@ -1,12 +1,14 @@
 var main = document.getElementById('main');
 window.customElements.define('memory-card', Card)
 
+
 var cards = generateCards();
 cards = shuffle(cards);
 
 function createBoard() {
     for (var i = 0; i < cards.length; i++) {
         var card = cards[i];
+        card.setAttribute("class","rounded-lg");
         main.appendChild(card);
         card.addEventListener('cardClicked', function (ev) {
             console.log(ev);
